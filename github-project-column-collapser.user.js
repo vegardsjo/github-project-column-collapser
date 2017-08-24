1e20d2db4e323424e1581b7fb06e5e2f89527ec1
Vegard Sjonfjell <vegardsjo@gmail.com>
2017-08-24 17:00:42 +0200

    Initial commit

---
+++ github-project-column-collapser.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         GitHub Project Column Collapser
+// @namespace    LoffeSoft
+// @version      0.9
+// @description  Collapses Columns in GitHub project view. Based on a bookmarklet written on a napkin by Daniele Grasso.
+// @author       Vegard Sjonfjell
+// @match        https://github.*.io/*
+// @match        https://github.com/*
+// @grant        none
+// ==/UserScript==
+
+function toggleHideChild(element) {
+    'use strict';
+    element.style.display = element.style.display === 'none' ? '' : 'none';
+}
+
+function toggleHideUi(column, colHead, colNameLabel) {
+    'use strict';
+    toggleHideChild(column.querySelector('.project-column'));
+    Array.from(colHead.children).filter(e => e.tagName != 'H4').forEach(e => toggleHideChild(e));
+    toggleHideChild(colNameLabel.querySelector('.counter'));
+}
+
+function collapseColumn(column) {
+    'use strict';
+    const colHead = column.querySelector('.project-column-header');
+    const colNameLabel = colHead.querySelector('h4');
+
+    toggleHideUi(column, colHead, colNameLabel);
+
+    const colHeadStyle = window.getComputedStyle(colHead);
+    const colStyle = window.getComputedStyle(column);
+
+    colNameLabel.style.writingMode = 'vertical-rl';
+    colHead.style.cssText = 'border-bottom: 1px solid rgb(229, 229, 229) !important'; // HACK: override !important higher up
+    colHead.style.borderRadius = '3px 3px 3px 3px';
+    colHead.style.width = '3em';
+    colHead.style.minHeight = '100%';
+    column.style.width = '4em';
+
+    column.dataset.collapsed = 'collapsed';
+}
+
+function unCollapseColumn(column) {
+    'use strict';
+    const colHead = column.querySelector('.project-column-header');
+    const colNameLabel = colHead.querySelector('h4');
+
+    toggleHideUi(column, colHead, colNameLabel);
+
+    colNameLabel.style.writingMode = '';
+    colHead.style.cssText = '';
+    column.style.width = '';
+
+    column.dataset.collapsed = '';
+}
+
+function toggleCollapseColumn(column) {
+    'use strict';
+    if (column.dataset.collapsed) {
+        unCollapseColumn(column);
+    } else {
+        collapseColumn(column);
+    }
+}
+
+(function() {
+    'use strict';
+    Array.from(document.querySelectorAll('.js-project-column'))
+        .forEach(
+        column => {
+            const colHead = column.querySelector('.project-column-header');
+            const colNameLabel = colHead.querySelector('h4');
+            colHead.addEventListener('click', function (event) {
+                if (event.target === this) {
+                    toggleCollapseColumn(column);
+                }
+            });
+
+            colNameLabel.addEventListener('click', (event) => toggleCollapseColumn(column));
+        });
+})();
