e2f769b0591e61ff32d768717f814c7a05917e9a
Vegard Sjonfjell <vegard.sjonfjell@schibsted.com>
2017-09-14 13:40:25 +0200

    Updated for GHE 2.10

--- github-project-column-collapser.user.js
+++ github-project-column-collapser.user.js
@@ -16,39 +16,39 @@ function toggleHideChild(element) {
 
 function toggleHideUi(column, colHead, colNameLabel) {
     'use strict';
-    toggleHideChild(column.querySelector('.project-column'));
+    toggleHideChild(column.querySelector('.js-project-column-cards'));
     Array.from(colHead.children).filter(e => e.tagName != 'H4').forEach(e => toggleHideChild(e));
-    toggleHideChild(colNameLabel.querySelector('.counter'));
+    toggleHideChild(colNameLabel.querySelector('.js-column-card-count'));
 }
 
 function collapseColumn(column) {
     'use strict';
-    const colHead = column.querySelector('.project-column-header');
+    const colHead = column.querySelector('.js-details-container');
     const colNameLabel = colHead.querySelector('h4');
 
     toggleHideUi(column, colHead, colNameLabel);
 
     const colHeadHeight = window.getComputedStyle(colHead).height;
     colNameLabel.style.writingMode = 'vertical-lr';
-    colHead.style.cssText = 'border-bottom: 1px solid rgb(229, 229, 229) !important'; // HACK: override !important higher up
+    colHead.style.cssText = 'border-bottom: 0 !important; padding-left: 8px !important; padding-right: 8px !important';
     colHead.style.borderRadius = '3px 3px 3px 3px';
-    colHead.style.width = '3em';
+    colHead.style.width = '2.8em';
     colHead.style.minHeight = `calc(100% + ${colHeadHeight})`;
-    column.style.width = '3.8em';
+    column.style.minWidth = '2.8em';
 
     column.dataset.collapsed = 'collapsed';
 }
 
 function unCollapseColumn(column) {
     'use strict';
-    const colHead = column.querySelector('.project-column-header');
+    const colHead = column.querySelector('.js-details-container');
     const colNameLabel = colHead.querySelector('h4');
 
     toggleHideUi(column, colHead, colNameLabel);
 
     colNameLabel.style.writingMode = '';
     colHead.style.cssText = '';
-    column.style.width = '';
+    column.style.minWidth = '';
 
     column.dataset.collapsed = '';
 }
@@ -67,7 +67,7 @@ function toggleCollapseColumn(column) {
     Array.from(document.querySelectorAll('.js-project-column'))
         .forEach(
         column => {
-            const colHead = column.querySelector('.project-column-header');
+            const colHead = column.querySelector('.js-details-container');
             const colNameLabel = colHead.querySelector('h4');
             colHead.addEventListener('click', function (event) {
                 if (event.target === this) {
